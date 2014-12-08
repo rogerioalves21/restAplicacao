d4a9def4eb314c0caf03d7c5ac6119896ec77f16
rogerioalves21 <rogerioalves21@10.0.2.7>
2014-12-07 23:12:44 -0200

    Pacote de testes

--- batch-processing/src/br/com/sicoob/cro/cop/batch/factory/Launchers.java
+++ batch-processing/src/br/com/sicoob/cro/cop/batch/factory/Launchers.java
@@ -12,7 +12,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Implementacoes de Launcher.
+ * Metodos utilitarios para trabalhar com launchers.
  *
  * @author Rogerio Alves Rodrigues
  */
