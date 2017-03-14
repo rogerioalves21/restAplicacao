44a2fac57e811857d034f85ea2237d71d0c89151
rogerioalves21 <rogerioalves21@10.0.2.7>
2017-03-13 23:08:13 -0300

    Projeto Rest. Com Json.

---
+++ rest-web-app/src/main/java/rest/web/app/Configuracao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest.web.app;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author rogerioalves21
+ */
+@ApplicationPath("servicos")
+public class Configuracao extends Application {
+    
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<Class<?>>();
+        classes.add(OlaMundoRs.class);
+        return classes;
+    }
+    
+}
---
+++ rest-web-app/src/main/java/rest/web/app/OlaMundoRs.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest.web.app;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author rogerioalves21
+ */
+@Path("/ola")
+public class OlaMundoRs {
+    
+    /*@GET
+    @Produces("text/plain")
+    public String olaMundo() {
+        return "Ola Mundo Rest!";
+    }*/
+    
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Login getLogin() {
+        Login login = new Login();
+        login.setLogin("rogerio.rodrigues");
+        login.setSenha("@Loureiro3");
+        return login;
+    }
+    
+}
